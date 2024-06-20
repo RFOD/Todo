9a6d134bd915ea4e873a9a24dfda60549b92926b
Rain <6rainforced9@gmail.com>
2024-06-20 14:30:29 +0300

    Made checkbox work/made the code more efficient

--- index.js
+++ index.js
@@ -5,39 +5,13 @@ checkboxes.forEach((checkbox) => {
 })
 function toggleCheckboxStatus(checkbox)
 {
-    const tasks = document.querySelectorAll('[data-index]')
-    
-    nrOfTasks = tasks.length - 1
-    if(orderCounter === nrOfTasks)
-        {
-            orderCounter = 0
-        }
-
     if(checkbox.classList.contains('bg-white'))
         {
+            checkbox.closest('[data-index]').classList.remove(`order-last`)
             checkbox.classList.toggle('bg-white')
             return
         }
-
-    tasks.forEach((task) => {
-    if(task.classList.contains('order-1'))
-        {
-            task.classList.remove('order-1')
-        }
-    else if(task.classList.contains('order-2'))
-        {
-            task.classList.remove('order-2')
-        }
-    else if(task.classList.contains('order-3'))
-        {
-            task.classList.remove('order-3')
-        }
-    else if(task.classList.contains('order-4'))
-        {
-            task.classList.remove('order-4')
-        }
-})
-    orderCounter++
     checkbox.classList.toggle('bg-white')
-    checkbox.closest('[data-index]').classList.add(`order-${orderCounter}`)
+    checkbox.closest('[data-index]').classList.add(`order-last`)
+
 }
